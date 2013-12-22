33352a42c6ae0e6c62ff9724458f6c2e8fad2f50
Pavel <pavel.evst@gmail.com>
2013-12-22 22:30:41 +0700

    Integration example

--- examples/simple.js
+++ examples/simple.js
@@ -1,13 +1,15 @@
 var fetcher = require("../index");
 var fs = require('fs');
-var open = require('open')
+var open = require('open');
+var adsParser = require('google-search-ads-parser');
 
 var log = function (a) { console.log(JSON.stringify(a, null, 2)); }
 
 var session = new fetcher.session({}, function() {
-  this.fetchWords('veritrans indonesia', function(headers, html, extraData) {
-    log(headers);
-    log(extraData);
+  this.fetchWords('hotel jakarta', function(headers, html, extraData) {
+    log(adsParser.extended.parseString(html));
+    //log(headers);
+    //log(extraData);
 
     fs.writeFileSync(__dirname + '/simple.html', html);
     open('file://' + __dirname + '/simple.html');
